8e6d6a622c1385ea33b79190da6d4754813f7562
Bruno <brunomiguel1997dh@gmail.com>
2021-10-08 00:14:22 +0200

    correction du code et  organisation des dossier

--- deconnexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // COMMENCER LA SESSION
-$_SESSION = array(); // VIDER TOUTES LES VARIABLES DE SESSION
-session_destroy(); //DÉTRUIRE TOUTES LES DONNÉES
-header("Location: index.php"); //REDIRIGER L'UTILISATEUR VERS UNE AUTRE PAGE
-?>
--- index.php
+++ index.php
@@ -1,76 +1,37 @@
- <!--------------------------------------------------------------------------------CONNEXION A LA BASE DE DONNEES-------------------------------------------------------------------------------->
 <?php
- include 'gestionserveur/gestion-connexion-utilisateur.php';
- 
-?>
+require('controller/frontend.php');
 
-<!DOCTYPE html>
-        <html lang="fr">
-
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-           
-            <title>Connexion</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="style/style.css" />
-        </head>
-
-        <body>
- <!--------------------------------------------------------------------------------HEADER -------------------------------------------------------------------------------->
-            <header class="header">                 
-                 <a href="index.php"><img src="Images/logo_gbaf.png"></a>
-            </header>   
-            
-
- <!--------------------------------------------------------------------------------FORMULAIRE CONNEXION-------------------------------------------------------------------------------->
-             <div class="conteneur-formulaire"> 
-            
-            <form class="formulaire" method="POST" action="">
-                <h1>
-                    Connexion
-                </h1>
-           
-                <!-- CHAMP POUR TAPER LE NOM D'UTILISATEUR -->
-            <input type ="text" name ="utilisateurconnexion" placeholder="Nom d'utilisateur"/> </br></br>
-
-
-                <!-- CHAMP POUR TAPER LE MOT DE PASSE -->
-           <input type ="password" name ="mdpconnexion" placeholder="Mot de passe"/> </br></br>
-
-
-                <!-- BOUTTON POUR SE COONECTER -->
-           <input type ="submit" name ="boutton_connexion" value="Se connecter" class="boutton-formulaire"/> </br>
-           
-          
-                <!-- RECUPERATION DE MOT DE PASSE -->
-            <p>          
-                <a id="mdp-oublie" href="verification_compte.php">Mot de passe oublié ?</a>
-            </p>  
-
-
-                <!-- BOUTTON POUR CREER UN COMPTE -->
-          <a href="inscription.php"> <input type="button" value="Créer un compte" class="boutton-formulaire"></a>
-          
-
-            </form> </br>
-            
- <!--------------------------------------------------------------------------------GESTION D'ERREUR-------------------------------------------------------------------------------->
-            <?php 
-        if (isset($erreur))
-        {       
-        echo  '<font color="red">'.$erreur."</font>";
+try {
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listPosts') {
+            listPosts();
         }
-            ?>
-
-      
-           
-             </div> 
-
-        </div>
-
-      
-
-        </body>
-
-        </html>
+        elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                post();
+            }
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+        elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                }
+                else {
+                    throw new Exception('Tous les champs ne sont pas remplis !');
+                }
+            }
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+    }
+    else {
+        listPosts();
+    }
+}
+catch(Exception $e) {
+    echo 'Erreur : ' . $e->getMessage();
+}
